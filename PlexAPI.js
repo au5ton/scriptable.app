cde002c0e3cb514aac8f0d012c0dce6a29f66014
Austin Jackson <themacphage@gmail.com>
2020-10-16 16:52:36 -0500

    draft

--- PlexAPI.js
+++ PlexAPI.js
@@ -1,6 +1,6 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
-// icon-color: deep-brown; icon-glyph: magic;
+// icon-color: orange; icon-glyph: file-video;
 module.exports = class PlexAPI {
     constructor(plexHost, plexToken) {
         this.plexHost = plexHost; // 123.123.123.123:45678
