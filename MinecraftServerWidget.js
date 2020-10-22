0ee638b8acb28daea1aacc7f7c06f25c8a3a3fbb
Austin Jackson <themacphage@gmail.com>
2020-10-22 15:44:02 -0500

    update

--- MinecraftServerWidget.js
+++ MinecraftServerWidget.js
@@ -10,30 +10,7 @@ async function getServerData(address) {
 
   // Number of players allowed in the player preview area
   const PLAYER_LIMIT = 8;
-  //const playerList = res.online && res.players.uuid ? Object.keys(res.players.uuid).map(e => { return { username: e, uuid: res.players.uuid[e] }}) : [];
-
-  const playerList = [
-    // { username: 'auston', uuid: '5e0b134e-dfd2-46e8-99af-2ecfe272970c' },
-    // { username: 'heber1', uuid: 'da4bad99-509f-4c6a-8ad4-da8d32fbfd7e' },
-    // { username: 'Notch', uuid: '069a79f4-44e9-4726-a5be-fca90e38aaf5' },
-    // { username: 'jeb_', uuid: '853c80ef-3c37-49fd-aa49-938b674adae6' },
-    // { username: 'Dinnerbone', uuid: '61699b2e-d327-4a01-9f1e-0ea8c3f06bc6' },
-    // { username: 'Grumm', uuid: 'e6b5c088-0680-44df-9e1b-9bf11792291b' },
-    // { username: 'TheShinyG', uuid: '76a8b93b-7100-4248-ae17-c9aeb5436a9e' },
-    // { username: 'Honeydew', uuid: '8f9bc2ed-1bb5-41ed-91be-02625c76bd7f' },
-    // { username: 'Zoned4', uuid: '1f4e070d-0f91-4cad-8265-0607b5cf1a32' },
-    // { username: 'SethBling', uuid: '55a2e72e-0161-4c85-b191-a0b227ff758a' },
-    // { username: 'EthosLab', uuid: '4f41dcda-449a-46b7-8635-88979061fdd2' },
-    // { username: 'paulsoaresjr', uuid: '06d8a457-891e-4f99-9d5c-c1e2b33f3321' },
-    // { username: 'CaptainSparklez', uuid: '5f820c39-5883-4392-b174-3125ac05e38c' },
-    // { username: 'FVDisco', uuid: '31feb2c6-1394-4de8-b418-e6b217dd2d04' },
-    // { username: 'Grumm', uuid: 'e6b5c088-0680-44df-9e1b-9bf11792291b' }
-    // { username: 'End_Knight', uuid: 'c6b802f0-da83-4e63-bda1-28dde3a62d4f' },
-    // { username: 'FluffThePanda', uuid: '48d4d27c-3d56-4b97-bf2e-b960ebf5e985' },
-    // { username: 'petfeesh', uuid: 'c2cfc2f8-e60b-4009-b3b7-35e0a807e502' },
-    // { username: 'medumdum', uuid: '550df348-aa9b-45dd-a2dd-473ef311d57a' },
-    // { username: 'robuttastic', uuid: '5a113935-22c5-48f3-b01b-92c61449ab84' }
-  ];
+  const playerList = res.online && res.players.uuid ? Object.keys(res.players.uuid).map(e => { return { username: e, uuid: res.players.uuid[e] }}) : [];
 
   // get player images
   for(let player of playerList) {
@@ -46,8 +23,7 @@ async function getServerData(address) {
     online: res.online,
     hostname: res.hostname || '',
     icon: res.icon || '',
-    //playersOnline: res.online ? res.players.online : 0,
-    playersOnline: playerList.length,
+    playersOnline: res.online ? res.players.online : 0,
     playersMax: res.online ? res.players.max : '???',
     playerList: playerList.slice(0, PLAYER_LIMIT),
     software: res.software ? res.software : 'Vanilla',
@@ -58,6 +34,7 @@ async function getServerData(address) {
 async function createWidget(computedValues) {
   const { version, motd, online, hostname, icon, playersOnline, playersMax, playerList, software } = computedValues;
 
+  // Gets the first color inside the MOTD because applying multiple colors to a single text is too complicated in Scriptable
   const getFirstColor = () => {
     try {
       return new Color(motd.html[0].match(hexRegex)[0]);
@@ -67,13 +44,15 @@ async function createWidget(computedValues) {
     }
   };
 
+  // Prepare some constants
   const hexRegex = /(?:#)(?:[a-f0-9]{3}|[a-f0-9]{6})\b/ig;
   const motdText = online ? motd.clean[0] : 'Server is offline';
   const firstColor = getFirstColor();
 
-  const grassBlockPng = Image.fromFile(`${FileManager.iCloud().documentsDirectory()}/MinecraftServerWidget/grass.png`);
+  const grassBlockPng = Image.fromFile(`${FileManager.iCloud().documentsDirectory()}/MinecraftServerWidget/background.png`);
   const serverIconPng = online ? Image.fromData(Data.fromBase64String(icon.split(',')[1])) : null;
 
+  // Setup widget
   let w = new ListWidget()
   w.backgroundImage = grassBlockPng;
   w.useDefaultPadding();
@@ -86,21 +65,28 @@ async function createWidget(computedValues) {
     logo.imageSize = new Size(40, 40);
   }
   header.addSpacer(null);
+  // Add server info to corner
   let corner = header.addStack();
   corner.layoutVertically();
   corner.spacing = 1;
+
+  // Server version
   let ver = corner.addText(`${version}`);
   ver.rightAlignText();
   ver.font = Font.boldSystemFont(12);
   ver.textColor = Color.white();
   ver.lineLimit = 1;
   ver.minimumScaleFactor = 0.35;
+
+  // Server players online
   let pmm = corner.addText(`${playersOnline} / ${playersMax}`);
   pmm.rightAlignText();
   pmm.font = Font.boldSystemFont(12);
   pmm.textColor = Color.white();
   pmm.lineLimit = 1;
   pmm.minimumScaleFactor = 0.35;
+
+  // If included, what kind of server
   if(software) {
     let soft = corner.addText(`${software} server`);
     soft.rightAlignText();
@@ -114,13 +100,14 @@ async function createWidget(computedValues) {
   w.addSpacer(null);
 
   if(playersOnline === 0) {
-    // Primary text is MOTD if no one is online
+    // if no one is online, primary content is MOTD
     let primaryText = w.addText(motdText);
     primaryText.font = Font.regularRoundedSystemFont(12);
     primaryText.textColor = firstColor;
   }
   else {
-    // Show logged in usernames
+    // if people are online, primary content is currently logged in players
+    // setup 2 columns
     playerStackParent = w.addStack();
     let playerStackLeft = playerStackParent.addStack();
     let playerStackRight = playerStackParent.addStack();
@@ -131,6 +118,7 @@ async function createWidget(computedValues) {
     playerStackRight.setPadding(0, 2, 0, 0);
     playerStackRight.spacing = 1;
 
+    // populate left column
     for(let i = 0; i < Math.floor(playerList.length/2); i++) {
       let player = playerList[i];
       let row = playerStackLeft.addStack();
@@ -145,6 +133,7 @@ async function createWidget(computedValues) {
       pName.minimumScaleFactor = 0.35;
     }
 
+    // populate right column
     for(let i = Math.ceil(playerList.length/2); i < playerList.length; i++) {
       let player = playerList[i];
       let row = playerStackRight.addStack();
@@ -159,29 +148,28 @@ async function createWidget(computedValues) {
       pName.minimumScaleFactor = 0.35;
     }
 
+    // If too many players to list them all, then show a message
     if(playersOnline > playerList.length) {
       let cont = w.addText(`+ ${playersOnline - playerList.length} more `);
       cont.centerAlignText();
       cont.font = Font.italicSystemFont(10);
       cont.textColor = new Color('#cccccc');
     }
-
   }
 
-  // spacer
-  w.addSpacer(null)
+  // Add spacer if we have no player overflow
+  if(playersOnline === playerList.length) w.addSpacer(null)
 
   // Add 'last updated' date to footer
   let footer = w.addStack();
   var nowDate = new Date();
   let date = footer.addDate(nowDate);
-  //date.applyRelativeStyle();
   date.applyTimeStyle();
   date.font = Font.footnote();
-  //date.font = Font.regularSystemFont(10);
   date.textColor = Color.white();
   date.leftAlignText();
 
+  // Add spacer for online indicator
   footer.addSpacer(null);
 
   if(online) {
--- MinecraftServerWidget/README.md
+++ MinecraftServerWidget/README.md
@@ -1,13 +1,18 @@
-# PlexStreamsWidget.js
+# MinecraftServerWidget.js
 
-<img src="preview.png" width="100">
+<img src="previews/preview_1.png" width="100">
+<img src="previews/preview_2.png" width="100">
+<br />
+<img src="previews/preview_3.png" width="205">
+<br />
+<img src="previews/in_game.png" width="205">
 
 ## Installation
 
-Copy `PlexStreamsWidget/` folder and `PlexStreamsWidget.js` to your iCloud Drive under the 'Scriptable' folder.
+Copy `MinecraftServerWidget/` folder and `MinecraftServerWidget.js` to your iCloud Drive under the 'Scriptable' folder.
 
-Add a small widget to your home screen and set the widget parameter text to a JSON string with your own values:
+Add a small widget to your home screen and set the widget parameter text to the publicly accessible address of the Minecraft server:
 
-```json
-{"PLEX_HOST": "192.168.1.2:32400", "PLEX_TOKEN": "laksdhjalskd"}
+```
+mc.example.com:25565
 ```
