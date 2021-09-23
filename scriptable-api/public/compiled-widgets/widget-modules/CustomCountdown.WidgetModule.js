e2eb75e3bd9325807cfbb0ba033845ad459c6da1
Austin Jackson <themacphage@gmail.com>
2021-09-22 19:40:36 -0500

    added custom countdown

---
+++ scriptable-api/public/compiled-widgets/widget-modules/CustomCountdown.WidgetModule.js
@@ -0,0 +1,83 @@
+(function () {
+
+    // Based on "Apple Event Calendar" by @JacobEthanWhite
+    const widgetModule = {
+        createWidget: async (params) => {
+            // extract user data
+            const { date, title, emoji, startColor: startColorHex, endColor: endColorHex } = parseWidgetParameter(params.widgetParameter);
+            const countDownDate = new Date(date).getTime();
+            const now = new Date().getTime();
+            const distance = countDownDate - now;
+            const days = Math.ceil(distance / (1000 * 60 * 60 * 24));
+            let w = new ListWidget();
+            let logo = w.addText(emoji);
+            logo.centerAlignText();
+            logo.font = Font.systemFont(30);
+            let titleText = w.addText(title);
+            titleText.font = Font.semiboldSystemFont(18);
+            titleText.centerAlignText();
+            titleText.textColor = Color.white();
+            let daysText = w.addText(days.toString(10));
+            daysText.font = Font.semiboldSystemFont(35);
+            daysText.textColor = Color.white();
+            let daysText2 = w.addText('days');
+            daysText2.font = Font.semiboldSystemFont(20);
+            daysText2.textColor = Color.white();
+            titleText.centerAlignText();
+            daysText.centerAlignText();
+            daysText2.centerAlignText();
+            //make a gradient
+            let startColor = new Color(startColorHex, 1);
+            let endColor = new Color(endColorHex, 1);
+            let gradient = new LinearGradient();
+            gradient.colors = [startColor, endColor];
+            gradient.locations = [0.0, 1];
+            w.backgroundGradient = gradient;
+            //w.backgroundColor = new Color("#ff5401")
+            return w;
+        }
+    };
+    const parseWidgetParameter = (param) => {
+        // handles: date:2021-10-22,title:Birthday,emoji:🍰,startColor:#f00,endColor:#050530
+        const paramParts = param
+            .split(',')
+            .map(e => e.includes(':') ? ({
+            key: e.substring(0, e.indexOf(':')).toLocaleLowerCase().trim(),
+            value: e.substring(e.indexOf(':') + 1).trim(),
+        }) : null);
+        const givenOptions = {};
+        for (let pair of paramParts) {
+            if (pair !== null) {
+                givenOptions[pair.key] = pair.value;
+            }
+        }
+        console.log('givenOptions');
+        console.log(givenOptions);
+        let _temp = new Date();
+        _temp.setDate(_temp.getDate() + 45);
+        let date = _temp.toISOString();
+        let title = 'Placeholder';
+        let emoji = '🔔';
+        let startColor = '#3050cc';
+        let endColor = '#050530';
+        if (givenOptions['date'] !== undefined)
+            date = givenOptions['date'];
+        if (givenOptions['title'] !== undefined)
+            title = givenOptions['title'];
+        if (givenOptions['emoji'] !== undefined)
+            emoji = givenOptions['emoji'];
+        if (givenOptions['startcolor'] !== undefined)
+            startColor = givenOptions['startcolor'];
+        if (givenOptions['endcolor'] !== undefined)
+            endColor = givenOptions['endcolor'];
+        return {
+            date,
+            title,
+            emoji,
+            startColor,
+            endColor,
+        };
+    };
+    module.exports = widgetModule;
+
+}());
